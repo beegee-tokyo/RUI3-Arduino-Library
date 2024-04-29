93bd784aaccfa30251635c6424db85d737f62e33
beegee-tokyo <bernd@giesecke.tk>
2024-04-29 17:23:37 +0800

    Still working to get the Github pages to work.

--- docs/examples.js
+++ docs/examples.js
@@ -2,8 +2,5 @@ var examples =
 [
     [ "RUI3-AT-ABP.ino", "d0/def/_r_u_i3-_a_t-_a_b_p_8ino-example.html", null ],
     [ "RUI3-AT-OTAA.ino", "de/dce/_r_u_i3-_a_t-_o_t_a_a_8ino-example.html", null ],
-    [ "RUI3-AT-P2P.ino", "d0/d7f/_r_u_i3-_a_t-_p2_p_8ino-example.html", null ],
-    [ "RUI3-AT-ABP-PIO.ino", "d1/d2f/_r_u_i3-_a_t-_a_b_p-_p_i_o_8ino-example.html", null ],
-    [ "RUI3-AT-OTAA-PIO.ino", "d4/dc4/_r_u_i3-_a_t-_o_t_a_a-_p_i_o_8ino-example.html", null ],
-    [ "RUI3-AT-P2P-PIO.ino", "d3/d3f/_r_u_i3-_a_t-_p2_p-_p_i_o_8ino-example.html", null ]
+    [ "RUI3-AT-P2P.ino", "d0/d7f/_r_u_i3-_a_t-_p2_p_8ino-example.html", null ]
 ];
